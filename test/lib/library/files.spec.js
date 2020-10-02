8c95faff489cd0130440fce2009d16d8e5038848
Davis Marques <dmarq.ezz@gmail.com>
2020-10-03 01:26:02 +1000

    Add test for library.update command

--- test/lib/library/files.spec.js
+++ test/lib/library/files.spec.js
@@ -1,19 +1,37 @@
-const constants = require('../../../lib/constants');
-const db = require('../../../lib/library/database');
-const fs = require('fs');
+const { expect } = require('chai');
 const files = require('../../../lib/library/files');
 const path = require('path');
 
 describe('lib / library / files', function() {
 
     const fixtures = path.join(process.cwd(), 'test', 'fixtures');
+    const state1 = path.join(fixtures, 'state1');
+    const state2 = path.join(fixtures, 'state2');
 
-    xdescribe('exists', function() {
-        it('returns true if the file exists and can be read', function() {
-            fail();
+    describe('exists', function() {
+        it('returns true if the file exists and can be read', function(done) {
+            const p = path.join(fixtures, 'state1', 'test1.md');
+            files
+                .exists(p)
+                .then(function (e) {
+                    expect(e).to.be.true;
+                    done();
+                })
+                .catch(function (err) {
+                    fail(err);
+                });
         });
-        it('returns false if the file does not exist or can not be read', function() {
-            fail();
+        it('returns false if the file does not exist or can not be read', function(done) {
+            const p = path.join(fixtures, 'state1', 'does-not-exist.md');
+            files
+                .exists(p)
+                .then(function (e) {
+                    expect(e).to.be.false;
+                    done();
+                })
+                .catch(function (err) {
+                    fail(err);
+                });
         });
     });
 
@@ -24,22 +42,15 @@ describe('lib / library / files', function() {
     });
 
     xdescribe('getCurrentFiles', function() {
-
-        let state1 = path.join(fixtures, 'state1');
-        let state2 = path.join(fixtures, 'state2');
-
         afterEach(function(done) {
             
         });
-
         beforeEach(function(done) {
 
         });
-
         it('returns the list of file paths, hashes in the current index', function() {
             fail();
         });
-
     });
 
     xdescribe('getDeletedFiles', function() {
--- test/lib/library/index.spec.js
+++ test/lib/library/index.spec.js
@@ -1,24 +1,72 @@
-const constants = require('../../../lib/constants');
+const { expect } = require('chai');
+const { DEFAULT_DATABASE_FILENAME } = require('../../../lib/constants');
+const db = require('../../../lib/library/database');
 const fs = require('fs');
-const lib = require('../../../lib/library');
-const path = require('path');
+const fsutils = require('nodejs-fs-utils');
+const os = require('os');
+const { join, sep } = require('path');
+const library = require('../../../lib/library');
 
-xdescribe('lib / library / info', function() {
+describe('lib / library', function() {
 
-    it('gets the list of all files in the directory', function() {
-        fail();
+    const fixtures = join(process.cwd(), 'test', 'fixtures');
+    const state1 = join(fixtures, 'state1');
+    const state2 = join(fixtures, 'state2');
+
+    let tmp = null;
+
+    beforeEach(function(done) {
+        fs.mkdtemp(`${os.tmpdir()}${sep}test-`, function(err, dtemp) {
+            if (err) throw new Error(err);
+            tmp = dtemp;
+            done();
+        });
     });
-    it('gets the list of all files in the directory by extension', function() {
-        fail();
+
+    xdescribe('info', function() {
+        it('gets the list of all files in the directory', function() {
+            fail();
+        });
+        it('gets the list of all files in the directory by extension', function() {
+            fail();
+        });
+        it('gets the list of all files that have been deleted', function () {
+            fail();
+        });
+        it('gets the list of all files that have changed content', function() {
+            fail();
+        });
+        it('gets the list of all files that have changed location', function() {
+            fail();
+        });    
     });
-    it('gets the list of all files that have been deleted', function () {
-        fail();
+
+    describe('update', function() {
+
+        beforeEach(function(done) {
+            fsutils.copy(state1, tmp, function (err) {
+                if (err) console.error(err);
+                done();
+            });
+        });
+
+        it('updates the file index', function(done) {
+            const args = { path: tmp, database: join(tmp, DEFAULT_DATABASE_FILENAME) };
+            library
+                .update(args)
+                .then(function() {
+                    done();
+                })
+                .catch(function(err) {
+                    fail(err);
+                });
+        });    
     });
-    it('gets the list of all files that have changed content', function() {
-        fail();
-    });
-    it('gets the list of all files that have changed location', function() {
-        fail();
+
+    xdescribe('updateFile', function() {
+        it('gets the list of all files that have changed location', function() {
+            fail();
+        });    
     });
 
 });
